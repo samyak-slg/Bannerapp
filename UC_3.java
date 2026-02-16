41b2393e3d3845df2d5a1ecec42eb12ad91d1c1b
samyak-slg <samyakstv@gmail.com>
2026-02-16 14:01:17 +0530

    UC3 Assignment added

---
+++ UC_3.java
@@ -0,0 +1,11 @@
+public class UC_3 {
+    public static void main(String[] args) {
+        System.out.println(String.join("","  ***    ***    *****   ***** "));
+        System.out.println(String.join(""," *   *  *   *   *    *  *     "));
+        System.out.println(String.join("","*     **     *  *    *  *     "));
+        System.out.println(String.join("","*     **     *  *****   ***** "));
+        System.out.println(String.join("","*     **     *  *           * "));
+        System.out.println(String.join(""," *   *  *   *   *           * "));
+        System.out.println(String.join("","  ***    ***    *       ***** "));
+    }
+}
